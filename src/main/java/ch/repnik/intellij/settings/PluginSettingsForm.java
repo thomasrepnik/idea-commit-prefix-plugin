be70b89d9ca908eedec44efe5e559631889d0e3d
Thomas Repnik <thomas.repnik@gmx.ch>
2020-06-04 18:26:50 +0200

    limited delimiter to 5 characters

--- src/main/java/ch/repnik/intellij/settings/PluginSettingsForm.java
+++ src/main/java/ch/repnik/intellij/settings/PluginSettingsForm.java
@@ -1,10 +1,21 @@
 package ch.repnik.intellij.settings;
 
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class PluginSettingsForm {
     private JPanel mainPanel;
     private JTextField txtDelimiter;
+
+    public PluginSettingsForm(){
+        txtDelimiter.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (txtDelimiter.getText().length() >= 5 ) // limit textfield to 5 characters
+                    e.consume();
+            }
+        });
+    }
 
     public JPanel getPanel(){
         return mainPanel;
