c856e06ea3974b2922dedce37278f5f08356d323
Thomas Repnik <thomas.repnik@gmx.ch>
2022-09-27 23:25:31 +0200

    support refreshing branches & code cleanup

--- src/main/java/ch/repnik/intellij/GitBaseCheckinHandlerFactory.java
+++ src/main/java/ch/repnik/intellij/GitBaseCheckinHandlerFactory.java
@@ -1,6 +1,7 @@
 package ch.repnik.intellij;
 
 import com.intellij.openapi.vcs.CheckinProjectPanel;
+import com.intellij.openapi.vcs.changes.CommitContext;
 import com.intellij.openapi.vcs.checkin.CheckinHandler;
 import com.intellij.openapi.vcs.checkin.VcsCheckinHandlerFactory;
 import git4idea.GitVcs;
@@ -14,7 +15,7 @@ public class GitBaseCheckinHandlerFactory extends VcsCheckinHandlerFactory {
 
     @NotNull
     @Override
-    protected CheckinHandler createVcsHandler(CheckinProjectPanel panel) {
+    protected CheckinHandler createVcsHandler(@NotNull CheckinProjectPanel panel, @NotNull CommitContext commitContext) {
         return new CommitPrefixCheckinHandler(panel);
     }
 }
